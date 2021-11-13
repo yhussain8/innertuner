56fd1da76b78587c7d9473170d7da89bde34212b
Yousuf Hussain <yhussain@Yousufs-MacBook-Pro.local>
2021-11-13 14:01:18 -0500

    heroku test

--- controllers/users.js
+++ controllers/users.js
@@ -22,15 +22,17 @@ async function create(req, res) {
  async function login(req, res) {
    try {
       const user = await User.findOne({ email: req.body.email })
+      console.log(user)
       
       // check password. if it's bad throw an error.
-      if (!(await bcrypt.compare(req.body.password, user.password))) throw new Error()
+      if (!(await bcrypt.compare(req.body.password, user.password))) throw new Error('Password Error')
+
    
       // if we got to this line, password is ok. give user a new token.
       const token = jwt.sign({ user }, process.env.SECRET,{ expiresIn: '24h' })
       
       res.status(200).json(token)
-   } catch {
+   } catch (err) {
       res.status(400).json('Bad Credentials')
    }
 }
