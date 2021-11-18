c40a6ef20cf9a912d73da7e77ee08f332fad6223
luaroxa <luaroxadance@gmail.com>
2021-11-18 08:27:06 -0700

    User Profile

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,10 +1,8 @@
 //=> maybe have a simple page of “how to” for the app and we can tell them too go to user profile page to add their goals as first step.
 
-
 // Let's simplify the scenairo that user has to use all 3 habits.
 // Here in user profile, user will change the goal and increments.
 // should we ask the desinger for a quick layout for this?
-
 
 // preferably I want to have habit setting as components.
 //but the habitvalue stst comes from home page, can we send that to another child component?
@@ -15,29 +13,42 @@
 }
 // => for now I will write these out below
 
+//I don't think collapse is needed. We can display all 3 habit cards. We are only letting them decide on the goal.
+// to be "fancy" we could prompt a message or a new component box when all 3 goals are filled in by the user.
+// "Let's get started!"
 export default function UserProfile() {
   state = {
-    collapse: false,
-    titleText: "How is your day going?",
+    // collapse: false,
+    titleText: "abc",
     subtitleText: "Hope you have a wonderful day!",
+    initialGoal: 0,
   };
+
+
+  setInitialGoal = (userInput) => {
+    let this.initialGoal = this.props.habitValues.goal 
+    this.setState({this.initialGoal: userInput})
+  }
 
   return (
     <div id="UserProfile" className="border border-black">
       {/* noot sure if we really basic user info here anymore */}
 
-      <h1>Welcome user! Set your intentions here.</h1>
+      <h1>Welcome to InnerTunner userName! Set your intentions here.</h1>
 
       <div className="border border-black" id="MainHabit">
         <h2>Habit Category: {this.props.habitValues.name}</h2>
-        <h4>maybe a message why this habit is good for you?</h4>
+        {/* maybe a message why this habit is good for you?
+        ex) Resting is just as important as abc! Or something about how setting goal is half the battle to acheive your 
+        healthy future. */}
+        <h4>Encouraging message here</h4>
 
         <div className="border">
-          Set your goal
+          Goal:
           <label>
             <input
               className=""
-              id="goal"
+              id="waterGoal"
               name="NewInput"
               value={this.props.habitValues[0].goal}
               onChange={this.handleChange}
@@ -45,7 +56,47 @@ export default function UserProfile() {
           </label>
         </div>
 
-        <div>log out can go here? </div>
+        <div className="border">
+          Goal:
+          <label>
+            <input
+              className=""
+              id="sleepGoal"
+              name="NewInput"
+              value={this.props.habitValues[1].goal}
+              onChange={this.handleChange}
+            />
+          </label>
+        </div>
+
+        <div className="border">
+          Goal:
+          <label>
+            <input
+              className=""
+              id="exerciseGal"
+              name="NewInput"
+              value={this.props.habitValues[2].goal}
+              onChange={this.handleChange}
+            />
+          </label>
+        </div>
+
+{/* Button to submit all 3 goals after putting input for all 3  */}
+        <div className="" id="submiBtn">
+            <button onClick={this.state.setInitialGoal}>ADD</button>
+        </div>
+
+
+        <div className="border" id="thankYouMsg">
+          <h2>Great job! Let's get started!</h2>
+        </div>
+
+
+        <div className="border" id="logout">
+          log out can go here. 
+          {/* (if we decide to not keep it in the homepage )  */}
+          </div>
       </div>
     </div>
   );
