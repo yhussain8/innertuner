15aa33b940b72a5fa9e95e8453f4f6ff21e526d8
Yousuf Hussain <yhussain@Yousufs-MacBook-Pro.local>
2021-11-19 02:11:05 -0500

    delete unused files

--- controllers/userInputs.js
+++ controllers/userInputs.js
@@ -2,53 +2,49 @@ const User = require('../models/user.js')
 const UserInput = require('../models/userInput.js')
 
 async function updateMood(req, res) {
-   try {
-      await UserInput.deleteMany({
-         userId: req.body.userId,
-         date: new Date(req.body.date),
-         inputName: req.body.inputName
-      })
-      let userInput = await new UserInput(req.body)
-      await userInput.save()
-      res.status(200).json("Successfully updated user input data")
-   } catch (err) {
-      res.status(400).json("Error updating user input data")
-   }
+	try {
+		await UserInput.deleteMany({
+			userId: req.body.userId,
+			date: new Date(req.body.date),
+			inputName: req.body.inputName
+		})
+		let userInput = await new UserInput(req.body)
+		await userInput.save()
+		res.status(200).json("Successfully updated user input data")
+	} catch (err) {
+		res.status(400).json("Error updating user input data")
+	}
 }
 
 async function weeklyMood(req, res) {
-   try {
-      console.log('***WEEKLY MOOD***')
-      let weeklyMood = await UserInput.find({
-         userId: req.params.id,
-         date: {'$gte': req.body.weekStartDate, '$lte': req.body.weekEndDate},
-         inputName: 'mood'
-      }).sort({date: 1})
-      let weekDays = ['M', 'Tu', 'W', 'Th', 'F', 'Sa', 'Su']
-      let weekDayArray = [{'M': 0}, {'Tu': 0}, {'W': 0}, {'Th': 0}, {'F': 0}, {'Sa': 0}, {'Su': 0}]
-      let populatedArray = weeklyMood.map((dailyMood) => {
-         let day = weekDays[dailyMood.date.getDay()]
-         return {[day]: dailyMood.inputValue}
-      })
-      console.log('emptyArray', weekDayArray)
-      console.log('populatedArray', populatedArray)
-      for (let i = 0; i < populatedArray.length; i++) {
-         let object = populatedArray[i]
-         let key = Object.keys(object)[0]
-         let value = object[key]
-         for (let j = 0; j < weekDayArray.length; j++) {
-            let object2 = weekDayArray[j]
-            let key2 = Object.keys(object2)[0]
-            if (key2 === key) {
-               weekDayArray[j][key] = value
-            }
-         }
-      }
-      console.log('final array', weekDayArray)
-      res.status(200).json(weekDayArray)
-   } catch (err) {
-      res.status(400).json("Error fetching weekly mood data")
-   }
+	try {
+		let weeklyMood = await UserInput.find({
+			userId: req.params.id,
+			date: { '$gte': req.body.weekStartDate, '$lte': req.body.weekEndDate },
+			inputName: 'mood'
+		}).sort({ date: 1 })
+		let weekDays = ['M', 'Tu', 'W', 'Th', 'F', 'Sa', 'Su']
+		let weekDayArray = [{ 'M': 0 }, { 'Tu': 0 }, { 'W': 0 }, { 'Th': 0 }, { 'F': 0 }, { 'Sa': 0 }, { 'Su': 0 }]
+		let populatedArray = weeklyMood.map((dailyMood) => {
+			let day = weekDays[dailyMood.date.getDay()]
+			return { [day]: dailyMood.inputValue }
+		})
+		for (let i = 0; i < populatedArray.length; i++) {
+			let object = populatedArray[i]
+			let key = Object.keys(object)[0]
+			let value = object[key]
+			for (let j = 0; j < weekDayArray.length; j++) {
+				let object2 = weekDayArray[j]
+				let key2 = Object.keys(object2)[0]
+				if (key2 === key) {
+					weekDayArray[j][key] = value
+				}
+			}
+		}
+		res.status(200).json(weekDayArray)
+	} catch (err) {
+		res.status(400).json("Error fetching weekly mood data")
+	}
 }
 
-module.exports = {updateMood, weeklyMood}
+module.exports = { updateMood, weeklyMood }
--- controllers/users.js
+++ controllers/users.js
@@ -1,40 +1,28 @@
 const User = require('../models/user.js')
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
-
 const SALT_ROUNDS = 6
 
 async function create(req, res) {
-   try {
-      const hashedPassword = await bcrypt.hash(req.body.password, SALT_ROUNDS)
-      const user = await User.create({name: req.body.name, email:req.body.email, password:hashedPassword,})
-   
-      // creating a jwt: 
-      // the first parameter specifies what we want to put into the token (in this case, our user document)
-      // the second parameter is a "secret" code. This lets our server verify if an incoming jwt is legit or not.
-      const token = jwt.sign({ user }, process.env.SECRET, { expiresIn: '30d' })
-      res.status(200).json(token) // send it to the frontend
-   } catch (err) {
-      res.status(400).json(err)
-   }
- }
- 
-async function login(req, res) {
-   try {
-      const user = await User.findOne({ email: req.body.email })
-      console.log(user)
-      
-      // check password. if it's bad throw an error.
-      if (!(await bcrypt.compare(req.body.password, user.password))) throw new Error('Password Error')
-
-   
-      // if we got to this line, password is ok. give user a new token.
-      const token = jwt.sign({ user }, process.env.SECRET,{ expiresIn: '24h' })
-      
-      res.status(200).json(token)
-   } catch (err) {
-      res.status(400).json('Bad Credentials')
-   }
+	try {
+		const hashedPassword = await bcrypt.hash(req.body.password, SALT_ROUNDS)
+		const user = await User.create({ name: req.body.name, email: req.body.email, password: hashedPassword, })
+		const token = jwt.sign({ user }, process.env.SECRET, { expiresIn: '30d' })
+		res.status(200).json(token) // send it to the frontend
+	} catch (err) {
+		res.status(400).json(err)
+	}
 }
 
-module.exports = {create, login}
+async function login(req, res) {
+	try {
+		const user = await User.findOne({ email: req.body.email })
+		if (!(await bcrypt.compare(req.body.password, user.password))) throw new Error('Password Error')
+		const token = jwt.sign({ user }, process.env.SECRET, { expiresIn: '24h' })
+		res.status(200).json(token)
+	} catch (err) {
+		res.status(400).json('Bad Credentials')
+	}
+}
+
+module.exports = { create, login }
